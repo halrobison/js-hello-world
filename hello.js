2863c92a1ede6456356652918abaf8a30b387506
halrobison <halrobisoncreative@gmail.com>
2023-12-18 17:39:00 -0600

    now prints out Hello, World!

--- hello.js
+++ hello.js
@@ -4,19 +4,19 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log("Hello");
+console.log("Hello, World.");
 
 /*  
 Next, PLAY! 
 Modify your code on line 7 to try to accomplish the tasks listed below:
 
-   1. Change the message that is printed.
-   2. Figure out what the parentheses do. Will the code work without them?
-   3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")?
-   4. Remove the semi-colon, ;.
-   5. Print a number. (Bonus: Print two numbers added together).
-   6. Print multiple messages one after the other.
-   7. Print two messages on the same line.
-   8. Print a message that contains quote marks, such as Quoth the Raven "Nevermore".
+   1. Change the message that is printed. (Success)
+   2. Figure out what the parentheses do. Will the code work without them? (Error)
+   3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")? (Must use both to open and close, but had success with " and '.)
+   4. Remove the semi-colon, ;. (Success)
+   5. Print a number. (Bonus: Print two numbers added together). (Success if number is placed in parens, i.e., '2'. Can do arithmetic by removing quotation marks!)
+   6. Print multiple messages one after the other. (Success)
+   7. Print two messages on the same line. (Error with multiple quoted phrases in one line)
+   8. Print a message that contains quote marks, such as Quoth the Raven "Nevermore". (Error)
    9. Other. You choose!
 */
